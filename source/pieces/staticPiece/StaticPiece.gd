227f81cf7276a5e2c10f698dd882b931ae29c6e1
Denomycor <afonsoresteves@hotmail.com>
2022-02-13 16:33:23 +0000

    #3 Added comments and avaiable_move method

--- source/pieces/staticPiece/StaticPiece.gd
+++ source/pieces/staticPiece/StaticPiece.gd
@@ -1,10 +1,11 @@
 class_name StaticPiece
 extends Position2D
 
+# enums 
 enum Classes {ATTACK, UTILITY, RESOURCE, HQ, WALL, OTHER}
 enum Elements {FIRE, WATER, EARTH, WIND, LIGHTNING, VOID}
 
-
+# export private const specs for each piece
 export(Classes) var _piece_class :int setget ,get_piece_class
 export(Elements) var _element :int setget ,get_element
 export var _hp :int setget ,get_hp
@@ -12,20 +13,26 @@ export var _cost :int setget ,get_cost
 export var _piece_name :String setget ,get_piece_name
 export var _description :String setget ,get_description
 
+# public vars
 var status := []
 var current_hp :int
 
+# private vars
 var _board_pos :Vector2 setget ,get_board_pos
 var _team :int setget ,get_team
 
 
+# private funcs
 func _ready():
 	pass
-	
-	
-	
-	
-	
+
+
+# abstract funcs
+func _destroy():
+	pass
+
+
+# getters	
 func get_hp():
 	return _hp	
 
