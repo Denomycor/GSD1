cfde5f2043d9f2e638a92b7bd9fb52ef0ee43396
Denomycor <afonsoresteves@hotmail.com>
2022-02-13 17:28:37 +0000

    Added boardWallPiece abstraction and signal piece_clicked to staticPiece

--- source/pieces/staticPiece/StaticPiece.gd
+++ source/pieces/staticPiece/StaticPiece.gd
@@ -1,47 +1,37 @@
 class_name StaticPiece
-extends Position2D
+extends "res://source/pieces/boardWallPiece/BoardWallPiece.gd"
 
-# enums 
-enum Classes {ATTACK, UTILITY, RESOURCE, HQ, WALL, OTHER}
-enum Elements {FIRE, WATER, EARTH, WIND, LIGHTNING, VOID}
+#signals
+signal piece_clicked
 
 # export private const specs for each piece
-export(Classes) var _piece_class :int setget ,get_piece_class
-export(Elements) var _element :int setget ,get_element
-export var _hp :int setget ,get_hp
 export var _cost :int setget ,get_cost
 export var _piece_name :String setget ,get_piece_name
 export var _description :String setget ,get_description
+export var _hp :int setget ,get_hp
 
 # public vars
 var status := []
 var current_hp :int
 
-# private vars
-var _board_pos :Vector2 setget ,get_board_pos
-var _team :int setget ,get_team
-
+# onready vars
+onready var sprite := $Sprite
 
 # private funcs
 func _ready():
 	pass
 
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		if sprite.get_rect().has_point(to_local(event.position)):
+			emit_signal("piece_clicked", self)
 
 # abstract funcs
 func _destroy():
 	pass
 
 
-# getters	
-func get_hp():
-	return _hp	
-
-func get_piece_class():
-	return _piece_class
-
-func get_element():
-	return _element
-
+# getters
 func get_cost():
 	return _cost
 
@@ -50,9 +40,7 @@ func get_piece_name():
 
 func get_description():
 	return _description
+	
+func get_hp():
+	return _hp	
 
-func get_board_pos():
-	return _board_pos
-
-func get_team():
-	return _team
