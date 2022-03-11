4031bc5fd72b022fb8b127a4617a2802e3940ec9
Denomycor <afonsoresteves@hotmail.com>
2022-03-11 22:54:37 +0000

    Added PieceInfo Class

---
+++ source/pieces/PieceInfo.gd
@@ -0,0 +1,28 @@
+class_name PieceInfo
+extends Reference
+
+enum Elements {FIRE, WATER, EARTH, LIGHTNING, WIND, VOID, NONE}
+enum Types {ATTACK, UTILITY, RESOURCE, HQ, WALL, OTHER}
+
+var hp :int
+var move_range :int
+var sight_range :int
+var type :int
+var element :int
+var cost :int
+var name :String
+var description :String
+var texture :Texture
+var keywords :Array
+
+func init(hp, move_range, sight_range, type, element, cost, name, descrition, texture, keywords):
+	self.hp = hp
+	self.move_range = move_range
+	self.sight_range = sight_range
+	self.type = type
+	self.element = element
+	self.cost = cost
+	self.name = name
+	self.description = description
+	self.texture = texture
+	self.keywords = keywords
